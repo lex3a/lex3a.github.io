5ec8e2df4277bbe1c087c3aa1cf1c9820156e586
v.sokolov <v.sokolov@timeweb.ru>
2023-12-13 14:21:35 +0300

    feat: directus code convert

---
+++ directus/directus.js
@@ -0,0 +1,12 @@
+const from = document.getElementById('from');
+const to = document.getElementById('to');
+const language = document.getElementById('languages');
+const transform = document.getElementById('transform');
+
+function convertToDirectusCode(text, type) {
+    to.value = `<pre><code class=${type}>${text}</code></pre>`
+}
+
+function onTranslate() {
+    convertToDirectusCode(from.value, language.value);
+}
